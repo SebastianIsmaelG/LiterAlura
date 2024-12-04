9e3b226d2b3a7c484c54ca339d4813129cb7b47a
SebastianIsmaelG <gutierrezs.contacto@gmail.com>
2024-12-04 12:22:38 -0300

    Feat: Adding API Request and jackson dependencies

---
+++ src/main/java/com/sebastianismaelg/literalura/main/Main.java
@@ -0,0 +1,17 @@
+package com.sebastianismaelg.literalura.main;
+
+import com.sebastianismaelg.literalura.services.ConvertData;
+import com.sebastianismaelg.literalura.services.RequestAPI;
+
+import java.util.Scanner;
+
+public class Main {
+    //variables API
+
+    private final String URL = "https://gutendex.com/books";
+    Scanner write = new Scanner(System.in);
+    RequestAPI requestAPI = new RequestAPI();
+    ConvertData convertData = new ConvertData();
+
+    //Interface Loop
+}
---
+++ src/main/java/com/sebastianismaelg/literalura/services/ConvertData.java
@@ -0,0 +1,19 @@
+package com.sebastianismaelg.literalura.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class  ConvertData  {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    //@Override
+    public <T> T obtainData(String json, Class<T> tClass) {
+        try {
+            return objectMapper.readValue(json,tClass);
+
+        } catch (JsonProcessingException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+}
